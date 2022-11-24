883a9423f6c8f37a28e33d3aa34d3d37e805949c
mgckaled <marcelgckaled@gmail.com>
2022-11-23 22:21:44 -0300

    feat: add SRP model to category service

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,20 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+export class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const categoryAlredyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlredyExists) {
+      throw new Error("Category already exists");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
