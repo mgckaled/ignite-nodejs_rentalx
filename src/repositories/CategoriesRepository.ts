eefc7a57658631710d821cffcbd4d78557a82e94
mgckaled <marcelgckaled@gmail.com>
2022-11-24 14:36:05 -0300

    feat: Add LSP model to category interface

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,10 @@
 import { Category } from "../model/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-export class CategoriesRepository {
+export class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
   constructor() {
@@ -24,7 +23,7 @@ export class CategoriesRepository {
     return this.categories;
   }
 
-  findByName(name: string) {
+  findByName(name: string): Category | undefined {
     const category = this.categories.find((category) => category.name === name);
     return category;
   }
