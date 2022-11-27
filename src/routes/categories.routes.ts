a84c325ad2838079cc536adc680f2dec7aea3d6b
mgckaled <marcelgckaled@gmail.com>
2022-11-27 16:09:50 -0300

    feat: create upload files with multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
@@ -6,12 +7,23 @@ import { listCategoriesController } from "../modules/cars/useCases/listCategorie
 
 const categoriesRoutes = Router();
 
+const upload = multer({
+  dest: "./tmp",
+});
+
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+
+  return response.send();
 });
 
 export { categoriesRoutes };
